67b466f314b181dc672d40d69c5a5fb7ae96a5ed
Gtwiin <54364001+Gtwiin@users.noreply.github.com>
2021-03-10 11:30:19 +0100

    Update Prueba_motor_UPM_Racing_v1.ino

--- Prueba_motor_UPM_Racing_v1.ino
+++ Prueba_motor_UPM_Racing_v1.ino
@@ -12,6 +12,6 @@ void loop()
   digitalWrite(6, HIGH); /*Activo el motor*/
   digitalWrite(8, HIGH); /*Pongo el motor a girar en sentido horario poniendo el pin 8 en HIGH y el 7 en LOW*/
   digitalWrite(7, LOW);
-  analogWrite(5,80); /*Pongo como salida analógica la velocidad por el pin 5 Cuidado al subirlo por encima de 150 porque podría estar pasando de los 7V del motor*/
+  analogWrite(5,80); /*Pongo como salida analógica la velocidad por el pin 5 Cuidado al subirlo por encima de 150 porque podría estar pasando de los 7V del motor, hay que investigarlo*/
   /*La velocidad del motor es un número comprendido entre 0 y 255 que se envía a la monster mediante un Pin analógico*/
 }
